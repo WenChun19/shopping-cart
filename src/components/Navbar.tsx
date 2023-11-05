dbe87ec9185611133e0285a6fef34fb4e6a951f9
Hoh1996 <wenchun.sitegiant@gmail.com>
2023-11-05 16:11:08 +0800

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,45 @@
+import { Container, Nav, Navbar as NavbarBs, Button } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+  return (
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
+      <Container>
+        <Nav className="me-auto">
+          <Nav.Link to="/" as={NavLink}>
+            Home
+          </Nav.Link>
+          <Nav.Link to="/store" as={NavLink}>
+            Store
+          </Nav.Link>
+          <Nav.Link to="/about" as={NavLink}>
+            About
+          </Nav.Link>
+        </Nav>
+        <Button
+          variant="outline-primary"
+          className="rounded-circle"
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
+        >
+          cart
+          <div
+            className="rounded-circle bg-danger
+             d-flex justify-content-center
+          align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
+        </Button>
+      </Container>
+    </NavbarBs>
+  );
+};
